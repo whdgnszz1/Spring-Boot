263150663ce6d8b0d3d36262bb57868ef74e1195
whdgnszz1 <whdgnszz1@snu.ac.kr>
2024-02-24 12:12:59 +0900

    Setting: Create entity

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain.item;
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Category> categories = new ArrayList<>();
+}
